ffccdb99484933fbaad73ae18e1209729eb37228
dezzerd <kirill9955555@mail.ru>
2025-06-17 11:24:58 +0300

    FIX5

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,8 @@
 import sys
-import os
-# Добавляем корень проекта в пути поиска модулей
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
-from app import app  # Теперь импорт должен работать
+from app import app  # Теперь будет работать везде
 
 def test_example():
     assert 1 + 1 == 2  # Простейший тест для проверки
